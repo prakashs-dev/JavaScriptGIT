6dba0e6781d3dba1861509fd9f509e224a857306
prakashs-dev <spraks99@gmail.com>
2026-07-08 18:14:38 +0530

    inital message

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-console.log("Hello World");
-let count = 5;
-for (let i = 0; i < 10; i++) {
+console.log("hello");
+
+for (let i = 1; i < 10; i++) {
   console.log(i);
 }
-
-let count = 0;
-let index = 10;
